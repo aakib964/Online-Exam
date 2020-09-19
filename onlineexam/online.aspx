cc45e8da70805b73db415bf217f24f44281ff256
aakib964 <aakibshaikh957@gmail.com>
2020-09-19 12:06:56 +0530

    project done

---
+++ onlineexam/online.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="online.aspx.cs" Inherits="onlineexam.online" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+                <h1>
+                    Online exam
+                </h1>           
+            <asp:Repeater ID="Repeater1" runat="server" >
+                <ItemTemplate>
+                    <asp:Table ID="Table1" runat="server">
+                    
+                        <asp:TableRow>
+                            <asp:TableCell>
+                                <%#Eval("qid")%> :    <%#Eval("question")%> 
+                            </asp:TableCell>
+                        </asp:TableRow>
+                        <asp:TableRow>
+                            <asp:TableCell>
+                                <asp:RadioButton ID="RadBtn1" runat="server"  Text='<%#Eval("option1")%>' GroupName="exam" ></asp:RadioButton>
+                                <asp:RadioButton ID="RadBtn2" runat="server"  Text='<%#Eval("option2")%>' GroupName="exam"></asp:RadioButton>
+                                <asp:RadioButton ID="RadBtn3" runat="server"  Text='<%#Eval("option3")%>' GroupName="exam"></asp:RadioButton>
+                                <asp:RadioButton ID="RadBtn4" runat="server"  Text='<%#Eval("option4")%>' GroupName="exam"></asp:RadioButton>
+                                <br />
+                                <asp:Label ID="LabCorrectAns" runat="server" Text='<%#Eval("correctans")%>' Visible="false"></asp:Label>
+                            </asp:TableCell>
+                      </asp:TableRow>
+                        <asp:TableRow>
+                            <asp:TableCell>
+                                <asp:Label ID="User" runat="server" Text=""></asp:Label>
+                                
+                           </asp:TableCell>
+                         </asp:TableRow>
+                    </asp:Table>
+                   
+                </ItemTemplate>
+            </asp:Repeater>
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+             
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ onlineexam/online.aspx.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace onlineexam
+{
+    public partial class online : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string maincon = ConfigurationManager.ConnectionStrings["Mycon"].ConnectionString;
+                SqlConnection sqlcon = new SqlConnection(maincon);
+                string sqlquerry = "select * from onlineexam";
+
+
+                sqlcon.Open();
+                SqlCommand cmd = new SqlCommand(sqlquerry, sqlcon);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                Repeater1.DataSource = dt;
+                Repeater1.DataBind();
+                sqlcon.Close();
+
+
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            foreach(RepeaterItem ri in Repeater1.Items)
+            {
+                RadioButton rb1 = (RadioButton)ri.FindControl("RadBtn1");
+                Label labcorrectans = (Label)ri.FindControl("LabCorrectAns");
+                labcorrectans.Visible = true;
+                
+                if (rb1.Checked)
+                {
+                    
+                    if (rb1.Text.Equals(labcorrectans.Text))
+                    {
+                        count = count + 1;
+                        Label Userselectans = (Label)ri.FindControl("User");
+                        Userselectans.Text = "The Selected Anwer is" + rb1.Text.ToString();
+                        Userselectans.ForeColor = System.Drawing.Color.Green;
+                      
+                    }
+                    else
+                    {
+                        Label Wrongans = (Label)ri.FindControl("User");
+                       
+                        Wrongans.Text = "The Selected Anwer  " + rb1.Text.ToString() + " is wrong";
+                        Wrongans.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+            }
+
+            
+            foreach (RepeaterItem ri in Repeater1.Items)
+            {
+
+                RadioButton rb2 = (RadioButton)ri.FindControl("RadBtn2");
+                Label labcorrectans = (Label)ri.FindControl("LabCorrectAns");
+
+                labcorrectans.Visible = true;
+                if (rb2.Checked)
+                {
+                    if (rb2.Text.Equals(labcorrectans.Text))
+                    {
+                        count = count + 1;
+                        Label Userselectans = (Label)ri.FindControl("User");
+                        Userselectans.Text = "The Selected Anwer is" + rb2.Text.ToString();
+                        Userselectans.ForeColor = System.Drawing.Color.Green;
+                       
+                    }
+                    else
+                    {
+                        Label Wrongans = (Label)ri.FindControl("User");
+                        Wrongans.Text = "The Selected Anwer  " + rb2.Text.ToString() + " is wrong";
+                        Wrongans.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+
+            }
+            foreach (RepeaterItem ri in Repeater1.Items)
+            {
+
+                RadioButton rb3 = (RadioButton)ri.FindControl("RadBtn3");
+                Label labcorrectans = (Label)ri.FindControl("LabCorrectAns");
+                labcorrectans.Visible = true;
+                if (rb3.Checked)
+                {
+
+                    if (rb3.Text.Equals(labcorrectans.Text))
+                    {
+                        count = count + 1;
+                        Label Userselectans = (Label)ri.FindControl("User");
+                        Userselectans.Text = "The Selected Anwer is" + rb3.Text.ToString();
+                        Userselectans.ForeColor = System.Drawing.Color.Green;
+                        
+                    }
+                    else
+                    {
+                        Label Wrongans = (Label)ri.FindControl("User");
+                        Wrongans.Text = "The Selected Anwer  "+ rb3.Text.ToString() + " is wrong";
+                        Wrongans.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+
+            }
+            foreach (RepeaterItem ri in Repeater1.Items)
+            {
+
+                RadioButton rb4 = (RadioButton)ri.FindControl("RadBtn4");
+                Label labcorrectans = (Label)ri.FindControl("LabCorrectAns");
+
+                labcorrectans.Visible = true;
+                if (rb4.Checked)
+                {
+                    if (rb4.Text.Equals(labcorrectans.Text))
+                    {
+                        count = count + 1;
+                        Label Userselectans = (Label)ri.FindControl("User");
+                        Userselectans.Text = "The Selected Anwer is " + rb4.Text.ToString();
+                        Userselectans.ForeColor = System.Drawing.Color.Green;
+                        
+                    }
+                    else
+                    {
+
+                        Label Wrongans = (Label)ri.FindControl("User");
+                        Wrongans.Text = "The Selected Anwer  " + rb4.Text.ToString() + " is wrong";
+                        Wrongans.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+             
+
+            }
+            Response.Write("your score=" + count);
+            /* int count = 0;
+
+             foreach (RepeaterItem ri in Repeater1.Items)
+             {
+
+                 RadioButton r1 = (RadioButton)ri.FindControl("RadBtn1");
+                 Label labcorrectans = (Label)ri.FindControl("LabCorrectAns");
+                 RadioButton r2 = (RadioButton)ri.FindControl("RadBtn2");
+                 RadioButton r3 = (RadioButton)ri.FindControl("RadBtn3");
+                 RadioButton r4 = (RadioButton)ri.FindControl("RadBtn4");
+
+
+                 if (r1.Checked==true)
+                 {
+
+                     if (r1.Text.Equals(labcorrectans.Text))
+                     {
+                         count = count + 1;
+                     }
+
+                 }
+                 else if(r2.Checked==true)
+                 {
+                     Response.Write("in r1");
+                     if (r2.Text.Equals(labcorrectans.Text))
+                     {
+                         count = count + 1;
+                     }
+                 }
+
+                 else if (r3.Checked == true)
+                 {
+                     Response.Write("in r1");
+                     if (r3.Text.Equals(labcorrectans.Text))
+                     {
+                         count = count + 1;
+                     }
+                 }
+                 else if (r4.Checked == true)
+                 {
+                     Response.Write("in r1");
+                     if (r4.Text.Equals(labcorrectans.Text))
+                     {
+                         count = count + 1;
+                     }
+                 }
+                 else
+                 {
+                     Response.Write("not select");
+                 }
+
+
+
+             }
+
+
+             Response.Write("your score=" + count);*/
+        }
+
+        }   
+    }
+
+
+    
+    
+
